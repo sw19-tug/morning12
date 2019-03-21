3073111f7a6bfc506802ba048f5bcd67741f7ed9
Andreas Hofer <ahofer@student.tugraz.at>
2019-03-21 14:04:13 +0100

    NOTE-002 Added rudimentary function testing [HA,LB]

--- app/src/androidTest/java/com/twelve/morning/notebook/CreateNoteTest.java
+++ app/src/androidTest/java/com/twelve/morning/notebook/CreateNoteTest.java
@@ -14,6 +14,7 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.core.IsNot.not;
 
 
 @RunWith(AndroidJUnit4.class)
@@ -35,9 +36,25 @@ public class CreateNoteTest {
         onView(withId(R.id.et_note_body)).inRoot(RootMatchers.isPlatformPopup()).check(matches(isDisplayed()));
         onView(withId(R.id.bt_note_create_save)).inRoot(RootMatchers.isPlatformPopup()).check(matches(isDisplayed()));
         onView(withId(R.id.bt_note_create_cancel)).inRoot(RootMatchers.isPlatformPopup()).check(matches(isDisplayed()));
-
+    }
+    @Test
+    public void popUpWindowClose(){
+        onView(withId(R.id.bt_create)).perform(click());
+        onView(withId(R.id.rl_create_note)).inRoot(RootMatchers.isPlatformPopup()).check(matches(isDisplayed()));
+        onView(withId(R.id.bt_note_create_cancel)).perform(click()));
+        onView(withId(R.id.rl_create_note)).inRoot(RootMatchers.isPlatformPopup()).check(matches(not(isDisplayed())));
     }
 
-
-
+    @Test
+    public void checkInputFieldParsing(){
+        String title = "hello world";
+        String body = "body of hello world";
+        onView(withId(R.id.bt_create)).perform(click());
+        onView(withId(R.id.et_note_title)).inRoot(RootMatchers.isPlatformPopup()).perform(click());
+        onView(withId(R.id.et_note_title)).inRoot(RootMatchers.isPlatformPopup()).perform(clearText(),typeTextIntoFocusedView(title));
+        onView(withId(R.id.et_note_title)).inRoot(RootMatchers.isPlatformPopup()).check(matches(withText(title)));
+        onView(withId(R.id.et_note_body)).inRoot(RootMatchers.isPlatformPopup()).perform(click());
+        onView(withId(R.id.et_note_body)).inRoot(RootMatchers.isPlatformPopup()).perform(clearText(),typeTextIntoFocusedView(body));
+        onView(withId(R.id.et_note_body)).inRoot(RootMatchers.isPlatformPopup()).check(matches(withText(body)));
+    }
 }
