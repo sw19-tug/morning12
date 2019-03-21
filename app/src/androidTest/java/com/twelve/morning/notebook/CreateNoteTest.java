330f72bd1e15a3a33b7a08f1ade82c00091289c1
Bernhard Leder <bernhard.leder@student.tugraz.at>
2019-03-21 11:47:30 +0100

    NOTE-002 modified popUpShowsOnFabClick()-Test and implemented passing version for it [HA,LB]

--- app/src/androidTest/java/com/twelve/morning/notebook/CreateNoteTest.java
+++ app/src/androidTest/java/com/twelve/morning/notebook/CreateNoteTest.java
@@ -1,11 +1,9 @@
 package com.twelve.morning.notebook;
 
 
+import android.support.test.espresso.matcher.RootMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -13,12 +11,10 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.not;
+
 
 @RunWith(AndroidJUnit4.class)
 public class CreateNoteTest {
@@ -28,8 +24,7 @@ public class CreateNoteTest {
     @Test
     public void popUpShowsOnFabClick(){
         onView(withId(R.id.bt_create)).perform(click());
-        onView(withId(R.id.pw_create_note)).check(matches(isDisplayed()));
-
+        onView(withId(R.id.pw_create_note)).inRoot(RootMatchers.isPlatformPopup()).check(matches(isDisplayed()));
     }
 
 
--- app/src/main/java/com/twelve/morning/notebook/MainActivity.java
+++ app/src/main/java/com/twelve/morning/notebook/MainActivity.java
@@ -1,13 +1,45 @@
 package com.twelve.morning.notebook;
 
+import android.content.Context;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        handleNoteCreation();
+
+    }
+
+
+    private void handleNoteCreation(){
+        FloatingActionButton create_note_btn = findViewById(R.id.bt_create);
+        create_note_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LinearLayout view_group = MainActivity.this.findViewById(R.id.pw_create_note);
+                LayoutInflater layoutInflater = (LayoutInflater)
+                        MainActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View customView = layoutInflater.inflate(R.layout.popup_window, view_group);
+                System.out.println(customView);
+                PopupWindow popup_window = new PopupWindow(customView,
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT);
+                popup_window.showAtLocation(view, Gravity.CENTER, 0, 0);
+            }
+        });
     }
 }
+
