eaffb2fb3eca459c2260546049f749739652d46b
Juri Larch <juril@live.de>
2019-03-23 11:46:16 +0100

    NOTE-001-003 Added testcase for notes list view children [LJ,SM]

--- app/src/androidTest/java/com/twelve/morning/notebook/NoteOverviewTest.java
+++ app/src/androidTest/java/com/twelve/morning/notebook/NoteOverviewTest.java
@@ -5,10 +5,16 @@ import android.support.test.rule.ActivityTestRule;
 import org.junit.Rule;
 import org.junit.Test;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasChildCount;
+import static android.support.test.espresso.matcher.ViewMatchers.hasMinimumChildCount;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static org.hamcrest.core.AllOf.allOf;
+import static org.hamcrest.Matchers.anything;
 
 public class NoteOverviewTest {
 
@@ -23,5 +29,10 @@ public class NoteOverviewTest {
     @Test
     public void testNotesListViewIsDisplayed() {
         onView(withId(R.id.list_notes)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testNotesListViewContainsElement() {
+        onView(allOf(anything(), withParent(withId(R.id.list_notes)))).check(matches(isDisplayed()));
     }
 }
