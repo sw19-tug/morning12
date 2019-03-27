d74763e89c67eb2d5c0995108546ee7ddfcddfba
Juri Larch <juril@live.de>
2019-03-27 08:52:29 +0100

    NOTE-001-004 Added testcase for Note creation [LJ,SM]

--- app/src/androidTest/java/com/twelve/morning/notebook/NoteOverviewTest.java
+++ app/src/androidTest/java/com/twelve/morning/notebook/NoteOverviewTest.java
@@ -1,23 +1,17 @@
 package com.twelve.morning.notebook;
 
 import android.support.test.rule.ActivityTestRule;
-import android.support.v7.widget.AppCompatTextView;
 
 import org.junit.Rule;
 import org.junit.Test;
 
-import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.hasChildCount;
-import static android.support.test.espresso.matcher.ViewMatchers.hasMinimumChildCount;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.core.AllOf.allOf;
-import static org.hamcrest.core.AnyOf.anyOf;
-import static org.hamcrest.Matchers.anything;
 
 public class NoteOverviewTest {
 
@@ -37,5 +31,14 @@ public class NoteOverviewTest {
     @Test
     public void testNotesListViewContainsElement() {
         onView(withText("Minus one that's three, quick maths")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testNotesListAddNote() {
+        onView(withId(R.id.bt_create)).perform(click());
+        onView(withId(R.id.et_note_title)).perform(typeText("Pizza"));
+        onView(withId(R.id.et_note_body)).perform(typeText("- Sale Marina\n- Farina W220\n- Lievito di birra- Mozzarella di Bufala Campana DOP\n- Basilico fresco- Pomodori pelati\n- Olio extra vergine"));
+        onView(withId(R.id.bt_note_create_save)).perform(click());
+        onView(withText("Pizza")).check(matches(isDisplayed()));
     }
 }
