9af8aa0fff7c8aeef09edaf366f0ce12d5007239
ahmed <ahmedazab888@gmail.com>
2019-03-27 11:26:39 +0100

    NOTE-009 Test Export Button [AN,PP]

---
+++ app/src/androidTest/java/com/twelve/morning/notebook/NoteExportTest.java
@@ -0,0 +1,42 @@
+package com.twelve.morning.notebook;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class NoteExportTest {
+
+
+    @Rule
+    public ActivityTestRule<CreateNoteActivity> activityCreateNoteTestRule =
+            new ActivityTestRule<>(CreateNoteActivity.class);
+
+
+    @Test
+    public void checkVisibleButton() {
+        onView(withId(R.id.export_button)).check(matches(isDisplayed()));
+
+    }
+
+    @Test
+    public void checkCklickableButton() {
+        onView(withId(R.id.export_button)).perform(click());
+
+
+    }
+
+    @Test
+    public void checkCopress() {
+
+    }
+}
