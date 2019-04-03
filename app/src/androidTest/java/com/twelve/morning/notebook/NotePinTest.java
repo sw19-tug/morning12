ab03e4a7aaa09a95b77eec0b6ff62376e5b3420b
Bernhard Leder <bernhard.leder@student.tugraz.at>
2019-04-03 10:27:24 +0200

    NOTE-005-002 added checkbox test for pinning [LJ,LB]

--- app/src/androidTest/java/com/twelve/morning/notebook/NotePinTest.java
+++ app/src/androidTest/java/com/twelve/morning/notebook/NotePinTest.java
@@ -7,12 +7,15 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isNotChecked;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 public class NotePinTest {
 
@@ -32,5 +35,25 @@ public class NotePinTest {
 
         notes = activityTestRule.getActivity().adapter.getNotes();
         Assert.assertTrue(notes[0].getPinned() == true);
+    }
+
+    @Test
+    public void testpinCheckbox(){
+        String title_input = "dummyTitle";
+        String body_input = "dummyBody";
+        onView(withId(R.id.bt_create)).perform(click());
+        onView(withId(R.id.rl_create_note)).check(matches(isDisplayed()));
+        onView(withId(R.id.et_note_title)).perform(clearText(), typeText(title_input));
+        onView(withId(R.id.et_note_body)).perform(clearText(), typeText(body_input)).perform(closeSoftKeyboard());
+        onView(withId(R.id.et_note_title)).check(matches(withText(title_input)));
+        onView(withId(R.id.et_note_body)).check(matches(withText(body_input)));
+        onView(withId(R.id.bt_note_create_save)).perform(click());
+        onView(withId(R.id.bt_create)).check(matches(isDisplayed()));
+
+        // -----------------------actual pin test --------------------------------
+
+        onView(withId(R.id.cb_pinned)).check(matches(isDisplayed()));
+        onView(withId(R.id.cb_pinned)).check(matches(isNotChecked()));
+
     }
 }
