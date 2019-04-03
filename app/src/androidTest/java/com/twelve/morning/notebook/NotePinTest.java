c299ede509909120b140425cae0980ee6c699f9c
Bernhard Leder <bernhard.leder@student.tugraz.at>
2019-04-03 11:39:30 +0200

    NOTE-005-002 added pinned stays on top test [LJ,LB]

--- app/src/androidTest/java/com/twelve/morning/notebook/NotePinTest.java
+++ app/src/androidTest/java/com/twelve/morning/notebook/NotePinTest.java
@@ -1,12 +1,21 @@
 package com.twelve.morning.notebook;
 
+import android.app.LauncherActivity;
+import android.support.test.annotation.UiThreadTest;
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -17,7 +26,9 @@ import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isNotChecked;
 import static android.support.test.espresso.matcher.ViewMatchers.isChecked;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.anything;
 
 public class NotePinTest {
 
@@ -89,6 +100,31 @@ public class NotePinTest {
         notes = activityTestRule.getActivity().adapter.getNotes();
         Assert.assertTrue(notes[0].getPinned());
         onView(withId(R.id.cb_pinned)).check(matches(isChecked()));
+
+    }
+
+    @Test
+    public void testPinnedRemainsOnTop(){
+        DatabaseWrapper.getInstance().reset();
+        onView(withId(R.id.bt_create)).perform(click());
+        onView(withId(R.id.et_note_title)).perform(typeText("A"), closeSoftKeyboard());
+        onView(withId(R.id.bt_note_create_save)).perform(click());
+        onView(withId(R.id.bt_create)).perform(click());
+        onView(withId(R.id.et_note_title)).perform(typeText("B"), closeSoftKeyboard());
+        onView(withId(R.id.bt_note_create_save)).perform(click());
+
+        Note[] notes = activityTestRule.getActivity().adapter.getNotes();
+        Assert.assertFalse(notes[0].getPinned());
+        Assert.assertFalse(notes[1].getPinned());
+
+        onData(anything()).inAdapterView(withId(R.id.list_notes))
+                .atPosition(1).onChildView(withId(R.id.cb_pinned)).perform(click());
+
+        onData(anything()).inAdapterView(withId(R.id.list_notes))
+                .atPosition(1).onChildView(withId(R.id.cb_pinned)).check(matches(isChecked()));
+
+        onData(anything()).inAdapterView(withId(R.id.list_notes))
+                .atPosition(0).onChildView(withId(R.id.cb_pinned)).check(matches(isChecked()));
 
     }
 }
