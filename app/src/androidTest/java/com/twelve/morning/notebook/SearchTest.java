5eb5014b05e60d88ff3a7554239f9095907a40e9
Michael Steurer <m.steurer@student.tugraz.at>
2019-05-08 11:26:26 +0200

    NOTE-015 Testcase Search-bar visibility [MI,SM]

--- app/src/androidTest/java/com/twelve/morning/notebook/SearchTest.java
+++ app/src/androidTest/java/com/twelve/morning/notebook/SearchTest.java
@@ -7,46 +7,34 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import java.util.List;
-import java.util.Set;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
-public class NoteTaggingTest {
+public class SearchTest {
 
     @Rule
     public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void tagTest() {
-
+    public void searchBarTest() {
+        String title_input = "dummyTitle";
+        String body_input = "dummyBodydummyBodydummyBodydummyBodydummyBodydummyBody";
         onView(withId(R.id.bt_create)).perform(click());
-        onView(withId(R.id.et_note_title)).perform(typeText("A"), closeSoftKeyboard());
-        onView(withId(R.id.et_note_body)).perform(typeText("Body #yoappsucsass #heylookatmeimahashtag #thursdaycommits #livinglife #justandroidappthings"), closeSoftKeyboard());
+        onView(withId(R.id.et_note_title)).perform(clearText(), typeText(title_input));
+        onView(withId(R.id.et_note_body)).perform(clearText(), typeText(body_input))
+                .perform(closeSoftKeyboard());
         onView(withId(R.id.bt_note_create_save)).perform(click());
 
-        Note[] notes = activityTestRule.getActivity().adapter.getNotes();
-        Assert.assertTrue(notes.length > 0);
+        onView(withText(title_input)).perform(click());
 
-        Note note = notes[0];
-        /*
-        List<Tag> tags = note.getTags();
-
-        Assert.assertTrue(tags.size() > 0);
-
-        Tag t1 = tags.get(0);
-        Assert.assertTrue(t1.getContent().equals("#yoappsucsass"));
-        */
-    }
-
-    @Test
-    public void testPinning() {
-        String[] notes = TagManager.parse("#tag1 #tag2");
-        Assert.assertTrue(notes[0].equals("tag1") && notes[1].equals("tag2"));
+        onView(R.id.search_bar).check(matches(isDisplayed()));
     }
 }
