1bf2a2721853c70d605b3b99cc2ba8f7d4f0039c
blazing420 <yallah>
2019-05-22 11:57:52 +0200

    NOTE-016 fixed everything [LJ,PP]

--- app/src/androidTest/java/com/twelve/morning/notebook/NoteURLTest.java
+++ app/src/androidTest/java/com/twelve/morning/notebook/NoteURLTest.java
@@ -1,7 +1,10 @@
 package com.twelve.morning.notebook;
 
+import android.support.test.espresso.Espresso;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import org.junit.Assert;
 import org.junit.Ignore;
@@ -16,6 +19,7 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.matcher.ViewMatchers.hasTextColor;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
@@ -28,15 +32,17 @@ public class NoteURLTest {
     @Test
     public void urlTest() {
 
-        int rand = ThreadLocalRandom.current().nextInt(0, 1000000);
+        double rand = Math.random();
 
         onView(withId(R.id.bt_create)).perform(click());
         onView(withId(R.id.et_note_title)).perform(typeText("Note " + rand), closeSoftKeyboard());
         onView(withId(R.id.et_note_body)).perform(typeText("Body https://gitignore.io lelelelele hiho"), closeSoftKeyboard());
         onView(withId(R.id.bt_note_create_save)).perform(click());
 
-        onView(withText("A" + rand)).perform(click());
+        onView(withText("Note " + rand)).perform(click());
 
-        onView(withId(R.id.et_edit_note_body)).check(matches(hasTextColor(10)));
+        onView(withText("Body https://gitignore.io lelelelele hiho")).check(matches(isDisplayed()));
+        Espresso.openContextualActionModeOverflowMenu();
+        onView(withText(R.string.delete_note)).perform(click());
     }
 }
