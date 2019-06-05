3b3d2a1be81dbe4d5573d1e3002ef6914a17afa4
Bernhard Leder <bernhard.leder@student.tugraz.at>
2019-06-05 10:56:13 +0200

    NOTE-034 added permission request for location [LB,PP]

--- app/src/main/java/com/twelve/morning/notebook/CreateNoteActivity.java
+++ app/src/main/java/com/twelve/morning/notebook/CreateNoteActivity.java
@@ -1,12 +1,16 @@
 package com.twelve.morning.notebook;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -40,6 +44,9 @@ public class CreateNoteActivity extends AppCompatActivity {
 
     private FusedLocationProviderClient fusedLocationClient;
 
+    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 1234;
+
+    private Boolean location_permission_granted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,10 +72,26 @@ public class CreateNoteActivity extends AppCompatActivity {
 
         });
 
+        checkForLocationPermission();
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         finishCreateNoteActivity((Button) findViewById(R.id.bt_note_create_cancel));
         finishCreateNoteActivity((Button) findViewById(R.id.bt_note_create_save));
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    location_permission_granted = true;
+                }
+            }
+        }
+    }
+
 
     private void storeNote() {
         EditText title = CreateNoteActivity.this.findViewById(R.id.et_note_title);
@@ -91,55 +114,68 @@ public class CreateNoteActivity extends AppCompatActivity {
         DatabaseWrapper.getInstance().addNote(note_to_store);
         final Integer target_id = DatabaseWrapper.getInstance().lastAddedNoteKey();
 
-        if (ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this,
-                        Manifest.permission.ACCESS_COARSE_LOCATION) !=
-                        PackageManager.PERMISSION_GRANTED) {
-        } else {
+        updateLocationOnPermissionGranted(target_id);
+    }
+
+
+    @SuppressLint("MissingPermission")
+    private void updateLocationOnPermissionGranted(final Integer target_id){
+        if(location_permission_granted) {
             fusedLocationClient.getLastLocation().addOnSuccessListener(this,
-                    new OnSuccessListener<Location>() {
-                        @Override
-                        public void onSuccess(Location location) {
-                            // Got last known location. In some rare situations this can be null.
-                            if(location != null) {
-                                Note note_to_update = DatabaseWrapper.getInstance()
-                                        .getNoteById(target_id);
-                                if (note_to_update != null
-                                        && note_to_update.getId() == target_id){
+                new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if(location != null) {
+                            Note note_to_update = DatabaseWrapper.getInstance()
+                                    .getNoteById(target_id);
+                            if (note_to_update != null
+                                    && note_to_update.getId() == target_id){
 
-                                    try {
-                                        List<Address> addresses;
-                                        Geocoder geocoder = new Geocoder(getApplicationContext(),
-                                                Locale.getDefault());
-                                        addresses = geocoder.getFromLocation(location.getLatitude(),
-                                                location.getLongitude(), 1);
-                                        note_to_update.setAddress(addresses.get(0).
-                                                getAddressLine(0));
-                                        System.out.println(location.toString());
-                                        for (Address a: addresses){
-                                            System.out.println(a.toString());
-                                        }
-                                        note_to_update.setLocation(location);
-                                        DatabaseWrapper.getInstance().saveNote(note_to_update);
-                                    } catch(IOException ioe) {
-                                        note_to_update.setLocation(location);
-                                        DatabaseWrapper.getInstance().saveNote(note_to_update);
+                                try {
+                                    List<Address> addresses;
+                                    Geocoder geocoder = new Geocoder(getApplicationContext(),
+                                            Locale.getDefault());
+                                    addresses = geocoder.getFromLocation(location.getLatitude(),
+                                            location.getLongitude(), 1);
+                                    note_to_update.setAddress(addresses.get(0).
+                                            getAddressLine(0));
+                                    System.out.println(location.toString());
+                                    for (Address a: addresses){
+                                        System.out.println(a.toString());
                                     }
-
-
-
+                                    note_to_update.setLocation(location);
+                                    DatabaseWrapper.getInstance().saveNote(note_to_update);
+                                } catch(IOException ioe) {
+                                    note_to_update.setLocation(location);
+                                    DatabaseWrapper.getInstance().saveNote(note_to_update);
                                 }
 
                             }
+
                         }
+                    }
 
-                    });
+                });
         }
-
 
     }
 
+    private void checkForLocationPermission(){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                        PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_LOCATION);
+
+        } else {
+            location_permission_granted = true;
+        }
+    }
 
     private void finishCreateNoteActivity(final Button button){
         if(button.getId() == R.id.bt_note_create_cancel ||
